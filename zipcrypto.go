516bf7b051f8767c4db2848f2ab80d79b6a01eee
whirlvfx <51461732+whirlvfx@users.noreply.github.com>
2019-06-07 10:52:32 -0400

    Update zipcrypto.go

--- zipcrypto.go
+++ zipcrypto.go
@@ -1,24 +1,26 @@
 package zip
 
 import (
-	"io"
 	"bytes"
 	"hash/crc32"
+	"io"
 )
 
-type ZipCrypto struct {
+//Crypto - Crypto struct
+type Crypto struct {
 	password []byte
-	Keys [3]uint32
+	Keys     [3]uint32
 }
 
-func NewZipCrypto(passphrase []byte) *ZipCrypto {
-	z := &ZipCrypto{}
+//NewZipCrypto - NewZipCrypto function
+func NewZipCrypto(passphrase []byte) *Crypto {
+	z := &Crypto{}
 	z.password = passphrase
 	z.init()
 	return z
 }
 
-func (z *ZipCrypto) init() {
+func (z *Crypto) init() {
 	z.Keys[0] = 0x12345678
 	z.Keys[1] = 0x23456789
 	z.Keys[2] = 0x34567890
@@ -28,19 +30,21 @@ func (z *ZipCrypto) init() {
 	}
 }
 
-func (z *ZipCrypto) updateKeys(byteValue byte) {
-	z.Keys[0] = crc32update(z.Keys[0], byteValue);
-	z.Keys[1] += z.Keys[0] & 0xff;
-	z.Keys[1] = z.Keys[1] * 134775813 + 1;
-	z.Keys[2] = crc32update(z.Keys[2], (byte) (z.Keys[1] >> 24));
+func (z *Crypto) updateKeys(byteValue byte) {
+	z.Keys[0] = crc32update(z.Keys[0], byteValue)
+	z.Keys[1] += z.Keys[0] & 0xff
+	z.Keys[1] = z.Keys[1]*134775813 + 1
+	z.Keys[2] = crc32update(z.Keys[2], (byte)(z.Keys[1]>>24))
 }
 
-func (z *ZipCrypto) magicByte() byte {
-	var t uint32 = z.Keys[2] | 2
+//magicByte - magicByte function
+func (z *Crypto) magicByte() byte {
+	var t = z.Keys[2] | 2
 	return byte((t * (t ^ 1)) >> 8)
 }
 
-func (z *ZipCrypto) Encrypt(data []byte) []byte {
+//Encrypt - Encrypt function
+func (z *Crypto) Encrypt(data []byte) []byte {
 	length := len(data)
 	chiper := make([]byte, length)
 	for i := 0; i < length; i++ {
@@ -51,11 +55,12 @@ func (z *ZipCrypto) Encrypt(data []byte) []byte {
 	return chiper
 }
 
-func (z *ZipCrypto) Decrypt(chiper []byte) []byte {
+//Decrypt - Decrypt function
+func (z *Crypto) Decrypt(chiper []byte) []byte {
 	length := len(chiper)
 	plain := make([]byte, length)
 	for i, c := range chiper {
-		v := c ^ z.magicByte();
+		v := c ^ z.magicByte()
 		z.updateKeys(v)
 		plain[i] = v
 	}
@@ -63,10 +68,11 @@ func (z *ZipCrypto) Decrypt(chiper []byte) []byte {
 }
 
 func crc32update(pCrc32 uint32, bval byte) uint32 {
-	return crc32.IEEETable[(pCrc32 ^ uint32(bval)) & 0xff] ^ (pCrc32 >> 8)
+	return crc32.IEEETable[(pCrc32^uint32(bval))&0xff] ^ (pCrc32 >> 8)
 }
 
-func ZipCryptoDecryptor(r *io.SectionReader, password []byte) (*io.SectionReader, error) {
+//CryptoDecryptor - CryptoDecryptor function
+func CryptoDecryptor(r *io.SectionReader, password []byte) (*io.SectionReader, error) {
 	z := NewZipCrypto(password)
 	b := make([]byte, r.Size())
 
@@ -78,7 +84,7 @@ func ZipCryptoDecryptor(r *io.SectionReader, password []byte) (*io.SectionReader
 
 type zipCryptoWriter struct {
 	w     io.Writer
-	z     *ZipCrypto
+	z     *Crypto
 	first bool
 	fw    *fileWriter
 }
@@ -102,7 +108,8 @@ func (z *zipCryptoWriter) Write(p []byte) (n int, err error) {
 	return
 }
 
-func ZipCryptoEncryptor(i io.Writer, pass passwordFn, fw *fileWriter) (io.Writer, error)  {
+//CryptoEncryptor - function CryptoEncryptor
+func CryptoEncryptor(i io.Writer, pass passwordFn, fw *fileWriter) (io.Writer, error) {
 	z := NewZipCrypto(pass())
 	zc := &zipCryptoWriter{i, z, true, fw}
 	return zc, nil
